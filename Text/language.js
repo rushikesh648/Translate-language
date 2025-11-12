20bd70a75f8f1d4d3a78087c938c31d102aeef7e
Rushikesh k. Pundkar <73341691+rushikesh648@users.noreply.github.com>
2025-11-12 19:45:56 +0530

    Create language.js

---
+++ Text/language.js
@@ -0,0 +1,5 @@
+state = {
+    inputText: '',       // Holds text typed by the user
+    translatedText: '',  // Holds the text returned by the API
+    // ... other properties (languages, sourceLang, targetLang)
+};
